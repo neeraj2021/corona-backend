a87d514bcb0c6d9f598bf428c91d199b5008fc05
kumar-neeraj-001 <neeraj@flick2know.com>
2022-07-04 15:47:52 +0530

    remove

--- build/UserController.d.ts
+++
@@ -1,8 +0,0 @@
-import "reflect-metadata";
-export declare class UserController {
-    getAll(): string;
-    getOne(id: number): string;
-    post(user: any): string;
-    put(id: number, user: any): string;
-    remove(id: number): string;
-}
